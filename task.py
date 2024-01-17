4fdf2988d956f7c943b1463aa9bc5df118303481
binfurqann <156215975+binfurqann@users.noreply.github.com>
2024-01-17 21:46:47 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,25 @@
+#Muhammad S/O Furqan
+#muhammadfurqanahmedmamji@gmail.com--------03212762993
+
+
+#Q1 - Write a Python program to do arithmetical operations addition and division
+#Taking Input
+number1=int(input("Enter Your First Number"))
+number2=int(input("Enter Your Second Number"))
+
+#Printing Output
+print("First number is:number1")
+print("Second number is:number2")
+a= number1+number2 
+print(a)
+
+#Q1 - Write a Python program to do arithmetical operations addition and division
+#Taking Input
+number1=int(input("Enter Your First Number"))
+number2=int(input("Enter Your Second Number"))
+
+#Printing Output
+print("First number is:number1")
+print("Second number is:number2")
+a= number1/number2 
+print(a)
